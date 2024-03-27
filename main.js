c0669e25a5ccdf3e41823cd9961f1e9b8cc7fa28
Aysel Qanbarova <ayselqanbarova0@gmail.com>
2024-03-27 20:03:58 +0400

    first js task

---
+++ main.js
@@ -0,0 +1,60 @@
+// // task1
+// let second = window.prompt("ENTER SECOND");
+// console.log(parseInt(second/60)+ " deqiqe "+(second%60)+" saniye");
+
+// task2
+// let temperatur =window.prompt("ENTER TEMP");
+// console.log(parseFloat(5/9 * (temperatur-32)));
+
+// task3
+// let a,b,c,d,e;
+// a=+window.prompt("enter num");
+// b=+window.prompt("enter num");
+// c=+window.prompt("enter num");
+// d=+window.prompt("enter num");
+// e=+window.prompt("enter num");
+// let result=(a+b+c+d+e)/5;
+// console.log(result);
+
+// task4
+// let birthDate=+window.prompt("Enter your Birthdate");
+// console.log(2024-birthDate);
+
+//task5
+// let a=window.prompt("enter num");
+// let b=window.prompt("enter num");
+// console.log(a*b/100);
+
+// task6
+// let a=window.prompt("enter num");
+// let b=window.prompt("enter num");
+// console.log(a*b/2);
+
+//task7
+// let credit=window.prompt("enter your credit");
+// let faiz=window.prompt("enter your parcent");
+// let ay=window.prompt("enter your month");
+// console.log((credit*faiz/100)/12);
+
+
+// task8
+// let firstName=window.prompt("Enter your fname");
+// let lastName=window.prompt("Enter your lname");
+// console.log("salam");
+
+
+// task9
+
+// let a=window.prompt("enter num");
+// let p=4*a;
+// let s=a**2;
+// console.log(p,s);
+
+// task10
+// let a=window.prompt("enter num1");
+// let b=window.prompt("enter num2");
+// let sum1=((a+b)**3);
+// let sum2=a**3+b**3;
+// console.log(sum1,sum2);
+
+
