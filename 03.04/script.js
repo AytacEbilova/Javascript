7daf0ab100e86c22c2946897538fed16f54ff9c6
Aysel Qanbarova <ayselqanbarova0@gmail.com>
2024-04-04 16:44:47 +0400

    new task added

---
+++ 03.04/script.js
@@ -0,0 +1,103 @@
+let arr = [
+  {
+    name: "test",
+    key: 1,
+  },
+  {
+    name: "task",
+    key: 2,
+  },
+  {
+    name: "tanqo",
+    key: 3,
+  },
+  {
+    name: "like",
+    key: 4,
+  },
+  {
+    name: "task",
+    key: 5,
+  },
+  {
+    name: "trust",
+    key: 6,
+  },
+  {
+    name: "test",
+    key: 7,
+  },
+  {
+    name: "last",
+    key: 8,
+  },
+  {
+    name: "tanqo",
+    key: 9,
+  },
+  {
+    name: "elephant",
+    key: 10,
+  },
+  {
+    name: "love",
+    key: 11,
+  },
+  {
+    name: "small",
+    key: 12,
+  },
+  {
+    name: "little",
+    key: 13,
+  },
+];
+// let count=0;
+// const res = arr.filter((item) => item.name.toLocaleLowerCase().startsWith("t"));
+// console.log(res);
+
+// arr.forEach((item) => {
+//     if(item.name.toLocaleLowerCase().startsWith("t") && item.name.toLocaleLowerCase().endsWith("t")) {
+//         count++;
+//     }
+// });
+
+// console.log(count)
+
+// const key=arr.filter((item) => item.name.toLocaleLowerCase().startsWith("t") && item.name.toLocaleLowerCase().endsWith("t"));
+// const sumKey=key.reduce((sum,item)=>sum+item.key,0);
+// console.log(sumKey);
+
+
+
+// arr.forEach((item) => {
+//     if(item.name.toLocaleLowerCase().endsWith("e")) {
+//         item.name="SuperDev"
+//     }
+// });
+// console.log(arr);
+
+// const sortedName=arr.toSorted((name1,name2)=>name1.name.length-name2.name.length,0);
+
+
+
+// console.log(sortedName[sortedName.length-1].name);
+// console.log(sortedName[sortedName.length-1].key);
+// console.log(arr.indexOf(sortedName[sortedName.length-1])**2);
+
+//task8
+let newArr = arr.filter(obj => obj.name.length === 4);
+console.log(newArr);
+//task9
+
+
+
+    
+
+
+
+
+
+
+ 
+
